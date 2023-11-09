921dfce9ad443e4d6f00fe294a0f0eeadd4ceb05
Gustavo Ochoa <200519@unsaac.edu.pe>
2023-11-09 18:40:03 -0500

    primera version header y about us

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Cambiar la opacidad del fondo del header al hacer scroll
+window.addEventListener("scroll", function() {
+    var scrollPosition = window.scrollY;
+    var header = document.querySelector("header");
+    var opacity = 0 + scrollPosition / 500; // Ajusta este valor según tus preferencias
+
+    // Limita la opacidad a un valor mínimo
+    if (opacity > 1) {
+        opacity = 1;
+    }
+
+    // Aplica la opacidad al fondo del header
+    header.style.backgroundColor = "rgba(51, 51, 51, " + opacity + ")";
+});
